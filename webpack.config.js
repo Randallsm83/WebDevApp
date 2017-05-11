3b859b5899d08982488634165bb47d5e2cca5313
Randall Miller <randallsm83@gmail.com>
2017-05-10 19:31:01 -0700

    Minor updates

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,28 @@ var BUILD_DIR = path.resolve(__dirname, 'dist');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
-  entry: APP_DIR + '/index.js',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          "babel-loader",
-          "eslint-loader"
-        ],
-      },
-    ],
-  },
-  watch: true
+	entry: APP_DIR + '/index.js',
+	output: {
+		path: BUILD_DIR,
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+				options: {
+					emitWarning: true,
+				}
+			},
+		],
+	},
+	watch: true
 }
